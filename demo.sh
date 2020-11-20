2c36dab735fafa774b82e1a88d03a04a497f9e61
baige <langer007@yeah.net>
2020-11-20 22:43:22 +0800

    create demo.sh to create a html program init framework

---
+++ demo.sh
@@ -0,0 +1,19 @@
+if [ -d $1 ]; then
+    echo 'Directory is exist'
+    exit 1
+fi
+if [ -f $1 ]; then
+    echo 'Create the $1 error! Please check wheather you had a file like the name!'
+    exit 1
+fi
+
+mkdir $1 && cd $1
+mkdir css && mkdir js
+echo '<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>' > index.js
+cd css
+echo 'h1{color: red;}' > style.css
+cd ../js
+echo 'var string = "Hello World"\nalert(string)' > main.js
+cd ../..
+echo "Success"
+exit 0
